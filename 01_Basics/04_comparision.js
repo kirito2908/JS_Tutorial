35ca52b3088a390c5b4d401bd1e4616cad42063a
user <user@users-MacBook-Pro.local>
2024-04-23 17:49:25 +0530

    sixth_lession

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,28 @@
+/*
+
+basic comparision :- 
+
+console.log( 2 > 1 );
+console.log( 2 >= 1 );
+console.log( 2 < 1 );
+console.log( 2 <= 1 );
+console.log( 2 == 1 );
+console.log( 2 != 1 );
+
+*/
+
+// console.log("2" < 1); // false, it'll convert "2" into number
+// console.log("2" == 2); // true
+
+// console.log(null > 0); // false
+// console.log(null >= 0); // false
+// console.log(null == 0); // true
+// because JS will convert null into 0 while comparing
+
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+// console.log(undefined == 0); // false
+// // no matter with what you compare undefind to, it'll always be false
+
+// console.log("2" == 2); // true
+// console.log("2" === 2); // false - it's called strick checking since we used === so it'll not convert any datatype to another and will compare as it is
