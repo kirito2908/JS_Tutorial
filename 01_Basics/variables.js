44d249b09aaed14e24cb80031fa8fbd5ca092b7d
user <user@users-MacBook-Pro.local>
2024-04-23 10:40:31 +0530

    second_lession

---
+++ 01_Basics/variables.js
@@ -0,0 +1,14 @@
+// In this lession we're going to learn about how many ways we can store values
+
+const myName = "Dhruvin";
+let myEmail = "dpghevariya27@gmail.com";
+var myPassword = "dhruvin2908";
+myLife = "Kim SongSun";
+
+// myName = "Kirito" // Not Allowed
+
+myEmail = "kazutokirigaya2908@gmail.com";
+myPassword = "kirito2908";
+myLife = "still Kim SongSun";
+
+console.table([myName, myEmail, myPassword, myLife]);
