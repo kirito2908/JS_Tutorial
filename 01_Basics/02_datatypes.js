3030ae7cc7136dc94f7fafa5f3542fba2b47c7e6
user <user@users-MacBook-Pro.local>
2024-04-23 23:26:02 +0530

    seventh_commit

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -1,14 +1,14 @@
-console.log(3 + 3); console.log("Dhruvin"); // Bad Practice
+// console.log(3 + 3); console.log("Dhruvin"); // Bad Practice
 
-console.log(3
-    +
-    3);
-console.log("Dhruvin"); // Another Bad Practice (Reduses Code Readability)
+// console.log(3
+//     +
+//     3);
+// console.log("Dhruvin"); // Another Bad Practice (Reduses Code Readability)
 
-console.log(3 + 3);
-console.log("Dhruvin"); // Correct Practice
+// console.log(3 + 3);
+// console.log("Dhruvin"); // Correct Practice
 
-const age = 22;
+// const age = 22;
 
 // Datatypes
 
@@ -28,6 +28,42 @@ Non-primitive
 
 */
 
-console.log(typeof age); // Output - number
-console.log(typeof undefined); // Output - undefined
-console.log(typeof null); // Output - object
+// console.log(typeof age); // Output - number
+// console.log(typeof undefined); // Output - undefined
+// console.log(typeof null); // Output - object
+
+// let id = Symbol("123");
+// let anotherId = Symbol("123");
+
+// Here we're giving same values to both id and anotherId but it'll be considered unique because of Symbol datatype
+
+// console.log(id === anotherId); // It'll be false
+
+// let bigNumber = 5457895920548506707n
+// console.log(typeof bigNumber); // It'll be BigInt
+
+
+
+/* ------------------------- Stack & Heap ------------------------- */
+
+// Data Related To All Primitive Datatypes Gets Stored In Stack And It Returns Copy Of It's Value When Asked
+// let myEmail = "dhruvin@gmail.com";
+// let yourEmail = myEmail;
+
+// yourEmail = "dhruvin2908@gmail.com";
+
+// console.log(myEmail); // It'll be the same as it was before - dhruvin@gmail.com
+// console.log(yourEmail); // It'll be as we had set it later on - dhruvin2908@gmail.com
+
+// Data Related To All Non-Primitive Datatypes Gets Stored In Heap And It Returns Original Reference Of The Data When Asked
+
+// let obj1 = {
+//     email: "dhruvin@gmail.com"
+// }
+// let obj2 = obj1;
+
+// obj2.email = "dhruvin2908@gmail.com";
+
+// console.log(obj1.email); 
+// console.log(obj2.email);
+// Both will be changed to dhruvin2908@gmail.com since we changed the original referenced data there
